03149f9d51f14fe313976fd408b0d2c9a5f4f4c1
Mary <mariyapiligrim3@gmail.com>
2021-10-14 22:01:29 +0300

    Add new text

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 const data = [
   {
     id: 1,
-    name: 'Laguna',
+    name: 'Lagoon Nebula',
     picture: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-lagoon-nebula.jpg',
-    text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptaterepellendus, alias deleniti porro minuquasi provident minimavoluptatem mollitia laborum'
+    text: 'The Lagoon Nebula, a vast stellar nursery located 4,000 light-years away, provides an extraordinary tapestry of star birth.'
   },
   {
     id: 2,
-    name: 'Reef',
+    name: 'The Cosmic Reef: NGC 2014 and NGC 2020',
     picture: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-cosmic-reef-2400x1200.jpg?t=tn2400',
-    text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptaterepellendus, alias deleniti porro minuquasi provident minimavoluptatem mollitia laborum'
+    text: 'Hubble reveals a colorful landscape of iridescent gases, streamers of dust, and brilliant, newborn stars in the giant red nebula NGC 2014. The blue ring of glowing oxygen at left is the neighboring nebula NGC 2020, formed by a torrential gaseous outflow from a lone, massive, super-hot star at its center.'
   },
   {
     id: 3,
     name: 'Saturn',
     picture: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-saturn.jpg?t=tn2400',
-    text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptaterepellendus, alias deleniti porro minuquasi provident minimavoluptatem mollitia laborum'
+    text: 'Hubble captured exquisite views of Saturn’s ring system and atmospheric details that once could only be provided by spacecraft visiting the distant world.'
   }
 ]
 
